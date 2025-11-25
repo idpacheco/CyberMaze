4616c412c0d13676ee9c64a70dabd7d9c5f8945f
isi-25 <idpacheco@uninorte.edu.co>
2025-11-24 21:41:32 -0500

    main menu fixed

--- proyecto_final/Feria_2025_30/mainMenu/Scenes/main.gd
+++ proyecto_final/Feria_2025_30/mainMenu/Scenes/main.gd
@@ -3,16 +3,18 @@ extends Control
 func _ready() -> void:
 	Global.index = -1
 	
-func _on_play_button_pressed() -> void:
-
+func _on_grafo_button_pressed() -> void:
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
 	AudioManager.SFXPlayer.play()
 	SceneTransitions.change_scene_to_file("res://seleccion.tscn")
-	pass # Replace with function body.
+
+func _on_arbol_button_pressed() -> void:
+	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
+	AudioManager.SFXPlayer.play()
+	SceneTransitions.change_scene_to_file("res://Select_Character/Scenes/select_character.tscn")
 
 
 func _on_settings_button_pressed() -> void:
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
 	AudioManager.SFXPlayer.play()
-	SceneTransitions.change_scene_to_file("res://Select_Character/Scenes/select_character.tscn")
-	pass # Replace with function body.
+	SceneTransitions.change_scene_to_file("res://mainMenu/Scenes/Config.tscn")

84e1f8028f69b70a67e0e49673d63c063289b86d
Sahory-24 <sahoryb@uninorte.edu.co>
2025-11-27 09:01:12 -0500

    change in etapa1

--- proyecto_final/Feria_2025_30/Mision_5/Etapa_4/MiniGame/Scenes/win.gd
+++ proyecto_final/Feria_2025_30/Mision_5/Etapa_4/MiniGame/Scenes/win.gd
@@ -6,5 +6,5 @@ func _on_continue_pressed() -> void:
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
 	AudioManager.SFXPlayer.play()
 	Global.index = 7
-	get_tree().quit()
+	SceneTransitions.change_scene_to_file("res://mainMenu/Scenes/main.tscn")
 	pass # Replace with function body.
